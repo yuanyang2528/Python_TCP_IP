5f0d8ea1c4a915433e479cf1c0348a0c6df45ea6
Yuanyang Ma <yuanyang2528@126.com>
2018-09-14 14:49:03 +0800

    Update server.py

--- server.py
+++ server.py
@@ -5,9 +5,9 @@ import _thread
 
 def tcplink(skt,addr):
   print(skt)
-  print(addr,"已经连接上...")
-  print('开始发送文件')
-  with open('./file.7z', 'rb') as f:
+  print(addr,"Connected succeed.")
+  print('Start send file...')
+  with open('./Project2.exe', 'rb') as f:
     for data in f:
       #print(data)
       skt.send(data)
@@ -25,8 +25,7 @@ def get_host_ip():
 	return ip
 
 HOST = get_host_ip()
-#HOST = "127.0.0.0"
-PORT = 9999
+PORT = 39576
 ADDR = (HOST,PORT)
 
 server = socket(AF_INET,SOCK_STREAM)
@@ -34,11 +33,11 @@ server.bind(ADDR)
 server.listen(5)
 
 while True:
-  print("等待连接...")
+  print("Wait connect...")
   skt,addr = server.accept()
   print(skt)
   try:
     _thread.start_new_thread(tcplink,(skt,addr))
   except:
-    print("线程无法启动")
+    print("Can't start thread.")
 server.close()

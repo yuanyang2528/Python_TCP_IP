56fb5fa3979dcd87ce92a2839f3d8294140ab2b7
Yuanyang Ma <yuanyang2528@126.com>
2018-09-14 14:47:26 +0800

    Add delete old files before receive the new files.

--- client.py
+++ client.py
@@ -1,23 +1,27 @@
 # _*_ utf-8 _*_
-
+import os
 from socket import *
 
 #Please use server ip address
 HOST = "127.0.0.0"
 
-PORT = 9999
+PORT = 39576
 ADDR = (HOST,PORT)
 
 client = socket(AF_INET,SOCK_STREAM)
 client.connect(ADDR)
-
-with open("./file.7z","ab") as f:
-  while True:
-    data = client.recv(1024)
-    if not data:
-      break;
-    f.write(data)
-
-f.close()
-print("接收完毕")
-client.close()
+try:
+	os.remove("./file.7z")
+	print("Delete old file succeed.")
+except:
+	print("Delete old file succeed.")
+finally:
+	with open("./file.7z","ab") as f:
+		while True:
+			data = client.recv(1024)
+			if not data:
+				break;
+			f.write(data)
+	f.close()
+	print("Received new file succeed.")
+	client.close()
